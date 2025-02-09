7fe1940de3963a5363241db19ff32aadbeff8a23
Luquinhas <lucmsilva651@gmail.com>
2025-02-09 08:19:34 -0300

    Added cleanup script for Derpfest

--- build/crave/14/derpfest.sh
+++ build/crave/14/derpfest.sh
@@ -1,4 +1,5 @@
 crave run --no-patch -- "curl https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/build/common_vars.sh | bash; \
+https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/cleanup/14/derpfest.sh | bash; \
 https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/build/dependencies/14/derpfest.sh | bash; \
 source build/envsetup.sh; \
 croot; \
---
+++ cleanup/14/derpfest.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+directories=(
+    "device/lineage/sepolicy"
+    "device/samsung_slsi/sepolicy"
+    "device/samsung/beyond0lte"
+    "device/samsung/exynos9820-common"
+    "vendor/samsung/beyond0lte"
+    "vendor/samsung/exynos9820-common"
+    "kernel/samsung/exynos9820"
+    "hardware/samsung"
+    "hardware/samsung_slsi-linaro/config"
+    "hardware/samsung_slsi-linaro/exynos"
+    "hardware/samsung_slsi-linaro/graphics"
+    "hardware/samsung_slsi-linaro/openmax"
+    "hardware/samsung_slsi-linaro/exynos5"
+    "hardware/samsung_slsi-linaro/interfaces"
+    "hardware/samsung_slsi-linaro/codec2"
+)
+for dir in "${directories[@]}"; do
+    if [ -d "$dir" ]; then
+        echo "======== removing dir $dir ========"
+        rm -rf "$dir"
+    fi
+done
+git clone https://github.com/LineageOS/android_device_lineage_sepolicy --depth 1 -b lineage-21.0 device/lineage/sepolicy
+git clone https://github.com/LineageOS/android_hardware_samsung --depth 1 -b lineage-21 hardware/samsung
+git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_config --depth 1 -b lineage-21 hardware/samsung_slsi-linaro/config
+git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_exynos --depth 1 -b lineage-21 hardware/samsung_slsi-linaro/exynos
+git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_graphics --depth 1 -b lineage-21 hardware/samsung_slsi-linaro/graphics
+git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_openmax --depth 1 -b lineage-21 hardware/samsung_slsi-linaro/openmax
+git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_exynos5 --depth 1 -b lineage-21 hardware/samsung_slsi-linaro/exynos5
+git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_interfaces --depth 1 -b lineage-21 hardware/samsung_slsi-linaro/interfaces
+git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_codec2 --depth 1 -b lineage-21 hardware/samsung_slsi-linaro/codec2
+git clone https://github.com/LineageOS/android_device_samsung_slsi_sepolicy --depth 1 -b lineage-21 device/samsung_slsi/sepolicy
+git clone https://github.com/lukibuilds/derpfest_device_samsung_beyond0lte/ --depth 1 -b fourteen device/samsung/beyond0lte
+git clone https://github.com/lukibuilds/derpfest_device_samsung_exynos9820-common --depth 1 -b fourteen device/samsung/exynos9820-common
+git clone https://github.com/linux4-bringup-priv/android_vendor_samsung_beyond0lte/ --depth 1 -b lineage-21.0 vendor/samsung/beyond0lte
+git clone https://github.com/linux4-bringup-priv/android_vendor_samsung_exynos9820-common/ --depth 1 -b lineage-21.0 vendor/samsung/exynos9820-common
+git clone https://github.com/lukibuilds/kernel_samsung_exynos9820/ --depth 1 -b lineage-21 kernel/samsung/exynos9820
+echo "======== cleanup finished ========"
