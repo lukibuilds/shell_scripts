a0a6b0ce4cd262183e16e92e3b54450de1101d46
Lucas Gabriel <lucmsilva651@gmail.com>
2025-02-09 17:04:38 -0300

    Forgot to add curl to command

--- build/crave/14/derpfest.sh
+++ build/crave/14/derpfest.sh
@@ -1,6 +1,6 @@
 crave run --no-patch -- "curl https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/build/common_vars.sh | bash; \
-https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/cleanup/14/derpfest.sh | bash; \
-https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/build/dependencies/14/derpfest.sh | bash; \
+curl https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/cleanup/14/derpfest.sh | bash; \
+curl https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/build/dependencies/14/derpfest.sh | bash; \
 source build/envsetup.sh; \
 croot; \
 lunch derp_beyond0lte-userdebug; \
