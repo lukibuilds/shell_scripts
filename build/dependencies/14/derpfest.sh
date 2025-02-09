878296d40dadeffdd9864cba3dc696fa5151eedb
Lucas Gabriel <lucmsilva651@gmail.com>
2025-02-08 22:07:25 -0300

    Update derpfest.sh

--- build/dependencies/14/derpfest.sh
+++ build/dependencies/14/derpfest.sh
@@ -8,3 +8,8 @@ git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_exyn
 git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_interfaces --depth 1 -b lineage-21 hardware/samsung_slsi-linaro/interfaces
 git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_codec2 --depth 1 -b lineage-21 hardware/samsung_slsi-linaro/codec2
 git clone https://github.com/LineageOS/android_device_samsung_slsi_sepolicy --depth 1 -b lineage-21 device/samsung_slsi/sepolicy
+git clone https://github.com/lukibuilds/derpfest_device_samsung_beyond0lte/ --depth 1 -b fourteen device/samsung/beyond0lte
+git clone https://github.com/lukibuilds/derpfest_device_samsung_exynos9820-common --depth 1 -b fourteen device/samsung/exynos9820-common
+git clone https://github.com/linux4-bringup-priv/android_vendor_samsung_beyond0lte/ --depth 1 -b lineage-21.0 vendor/samsung/beyond0lte
+git clone https://github.com/linux4-bringup-priv/android_vendor_samsung_exynos9820-common/ --depth 1 -b lineage-21.0 vendor/samsung/exynos9820-common
+git clone https://github.com/lukibuilds/kernel_samsung_exynos9820/ --depth 1 -b lineage-21 kernel/samsung/exynos9820
