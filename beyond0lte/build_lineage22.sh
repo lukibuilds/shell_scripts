e9363086beb99f00c30d50baf4808cb00d640fed
Luquinhas <lucmsilva651@gmail.com>
2025-02-09 17:39:44 -0300

    Source common vars instead of running the script

--- beyond0lte/build_lineage22.sh
+++ beyond0lte/build_lineage22.sh
@@ -1,4 +1,4 @@
-curl https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/common_vars.sh | bash
+source <(curl -s https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/common_vars.sh)
 curl https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/beyond0lte/dependencies_lineage22.sh | bash
 source build/envsetup.sh
 croot
