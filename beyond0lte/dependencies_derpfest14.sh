d01a8e144bcd267935fa2322a96dbe03ac468d92
Lucas Gabriel <lucmsilva651@gmail.com>
2025-02-13 16:18:15 -0300

    Update dependencies_derpfest14.sh

--- beyond0lte/dependencies_derpfest14.sh
+++ beyond0lte/dependencies_derpfest14.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 directories=(
-    "device/lineage/sepolicy"
+    "device/derp/sepolicy"
     "device/samsung_slsi/sepolicy"
     "device/samsung/beyond0lte"
     "device/samsung/exynos9820-common"
@@ -23,7 +23,7 @@ for dir in "${directories[@]}"; do
         rm -rf "$dir"
     fi
 done
-git clone https://github.com/LineageOS/android_device_lineage_sepolicy --depth 1 -b lineage-21.0 device/lineage/sepolicy
+git clone https://github.com/DerpFest-AOSP/device_derp_sepolicy --depth 1 -b lineage-21.0 device/derp/sepolicy
 git clone https://github.com/LineageOS/android_hardware_samsung --depth 1 -b lineage-21 hardware/samsung
 git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_config --depth 1 -b lineage-21 hardware/samsung_slsi-linaro/config
 git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_exynos --depth 1 -b lineage-21 hardware/samsung_slsi-linaro/exynos
@@ -33,7 +33,7 @@ git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_exyn
 git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_interfaces --depth 1 -b lineage-21 hardware/samsung_slsi-linaro/interfaces
 git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_codec2 --depth 1 -b lineage-21 hardware/samsung_slsi-linaro/codec2
 git clone https://github.com/LineageOS/android_device_samsung_slsi_sepolicy --depth 1 -b lineage-21 device/samsung_slsi/sepolicy
-git clone https://github.com/lukibuilds/derpfest_device_samsung_beyond0lte/ --depth 1 -b fourteen device/samsung/beyond0lte
+git clone https://github.com/lukibuilds/device_samsung_beyond0lte/ --depth 1 -b lineage-21 device/samsung/beyond0lte
 git clone https://github.com/lukibuilds/android_vendor_lineage-priv_keys --depth 1 -b main vendor/derp/signing/keys
 git clone https://github.com/lukibuilds/derpfest_device_samsung_exynos9820-common --depth 1 -b fourteen device/samsung/exynos9820-common
 git clone https://github.com/linux4-bringup-priv/android_vendor_samsung_beyond0lte/ --depth 1 -b lineage-21.0 vendor/samsung/beyond0lte
