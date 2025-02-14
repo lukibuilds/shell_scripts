13380b0ad3bf1777f7de1d24ee186b14bd74ac14
Lucas Gabriel <lucmsilva651@gmail.com>
2025-02-14 10:48:10 -0300

    Update dependencies_derpfest14.sh

--- beyond0lte/dependencies_derpfest14.sh
+++ beyond0lte/dependencies_derpfest14.sh
@@ -1,28 +1,3 @@
-#!/bin/bash
-directories=(
-    "device/derp/sepolicy"
-    "device/samsung_slsi/sepolicy"
-    "device/samsung/beyond0lte"
-    "device/samsung/exynos9820-common"
-    "vendor/samsung/beyond0lte"
-    "vendor/derp/signing/keys"
-    "vendor/samsung/exynos9820-common"
-    "kernel/samsung/exynos9820"
-    "hardware/samsung"
-    "hardware/samsung_slsi-linaro/config"
-    "hardware/samsung_slsi-linaro/exynos"
-    "hardware/samsung_slsi-linaro/graphics"
-    "hardware/samsung_slsi-linaro/openmax"
-    "hardware/samsung_slsi-linaro/exynos5"
-    "hardware/samsung_slsi-linaro/interfaces"
-    "hardware/samsung_slsi-linaro/codec2"
-)
-for dir in "${directories[@]}"; do
-    if [ -d "$dir" ]; then
-        echo "======== removing dir $dir ========"
-        rm -rf "$dir"
-    fi
-done
 git clone https://github.com/DerpFest-AOSP/device_derp_sepolicy --depth 1 -b lineage-21.0 device/derp/sepolicy
 git clone https://github.com/LineageOS/android_hardware_samsung --depth 1 -b lineage-21 hardware/samsung
 git clone https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_config --depth 1 -b lineage-21 hardware/samsung_slsi-linaro/config
@@ -39,4 +14,3 @@ git clone https://github.com/lukibuilds/device_samsung_exynos9820-common --depth
 git clone https://github.com/lukibuilds/vendor_samsung_beyond0lte/ --depth 1 -b lineage-21.0 vendor/samsung/beyond0lte
 git clone https://github.com/linux4-bringup-priv/android_vendor_samsung_exynos9820-common/ --depth 1 -b lineage-21.0 vendor/samsung/exynos9820-common
 git clone https://github.com/lukibuilds/kernel_samsung_exynos9820/ --depth 1 -b lineage-21 kernel/samsung/exynos9820
-echo "======== cleanup finished ========"
