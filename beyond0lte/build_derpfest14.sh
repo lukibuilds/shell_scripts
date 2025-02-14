1f4b5e86d41d86c25e40728b8b3dafbf46019c99
Luquinhas <lucmsilva651@gmail.com>
2025-02-14 00:03:02 -0300

    Suppress output of build cleanup script

--- beyond0lte/build_derpfest14.sh
+++ beyond0lte/build_derpfest14.sh
@@ -1,5 +1,5 @@
 source <(curl -s https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/common_vars.sh)
-curl https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/beyond0lte/dependencies_derpfest14.sh | bash
+curl https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/beyond0lte/dependencies_derpfest14.sh | bash > /dev/null 2>&1
 source build/envsetup.sh
 croot
 lunch derp_beyond0lte-userdebug
