8619974af7285418835c333c0db27340ef33cb38
Lucas Gabriel <lucmsilva651@gmail.com>
2025-02-14 10:49:20 -0300

    Update build_derpfest14.sh

--- beyond0lte/build_derpfest14.sh
+++ beyond0lte/build_derpfest14.sh
@@ -1,5 +1,6 @@
 source <(curl -s https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/common_vars.sh)
-curl https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/beyond0lte/dependencies_derpfest14.sh | bash > /dev/null 2>&1
+curl https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/beyond0lte/cleanup_derpfest14.sh | bash
+curl https://raw.githubusercontent.com/lukibuilds/shell_scripts/refs/heads/main/beyond0lte/dependencies_derpfest14.sh | bash
 source build/envsetup.sh
 croot
 lunch derp_beyond0lte-userdebug
